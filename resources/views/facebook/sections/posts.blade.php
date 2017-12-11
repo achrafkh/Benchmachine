7af5e6945e735d2f2292fab0d6c0fd167faa3346
Khouadja achraf <fb-achraf@hotmail.com>
2017-12-11 16:00:13 +0100

    test

--- resources/views/facebook/sections/posts.blade.php
+++ resources/views/facebook/sections/posts.blade.php
@@ -1,4 +1,4 @@
-<div class="section posts-section" data-aos="slide-up" data-aos-once="true">
+<div class="section posts-section" data-aos="slide-up" data-aos-once="true" data-aos-duration="800" data-aos-delay="200">
 	<div class="container">
 		<div class="section-header">
 			<h2 class="section-title">
--- resources/views/facebook/sections/table.blade.php
+++ resources/views/facebook/sections/table.blade.php
@@ -1,4 +1,4 @@
-<div class="section table-section" data-aos="fade-up" data-aos-once="true" data-aos-duration="400">
+<div class="section table-section" data-aos="fade-up" data-aos-once="true" data-aos-duration="800" data-aos-delay="200">
 	<div class="container" >
 		<div class="section-header">
 			<h2 class="section-title">
