f13daa84d2ae479769a216c7f942981b7693c6ad
Khouadja achraf <fb-achraf@hotmail.com>
2017-12-21 17:31:41 +0100

    Added meta data

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,42 @@
     <meta charset="UTF-8">
     <meta name="theme-color" content="#0363c5" />
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1">
-
+    <meta name="keywords" content="Benchmarks, Facebook, KPI, Social benchmarking">
+    <meta name="description" content="Discover your key performances indicators, Learn from social leaders and Set goals and baselines for performance and growth based on your direct competitors.">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" value="Discover your key performances indicators, Learn from social leaders and Set goals and baselines for performance and growth based on your direct competitors.">
+
+
+
+
+        <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="The Name or Title Here">
+    <meta itemprop="description" content="This is the page description">
+    <meta itemprop="image" content="http://www.example.com/image.jpg">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="{{ url('/images/logo.jpg') }}">
+    <meta name="twitter:site" content="@Kpeiz_Digital">
+    <meta name="twitter:title" content="Benchmarks inc">
+    <meta name="twitter:description" content="Discover your key performances indicators, Learn from social leaders and Set goals and baselines for performance and growth based on your direct competitors.">
+    <meta name="twitter:creator" content="@Kpeiz_Digital">
+
+    <meta name="twitter:image:src" content="{{ url('/assets/images/christmas-bg.jpg') }}">
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Benchmarks.digital" />
+    <meta property="og:type" content="Landing Page" />
+    <meta property="og:url" content="{{ url('/') }}" />
+    <meta property="og:image" content="{{ url('/images/logo.jpg') }}" />
+    <meta property="og:description" content="Discover your key performances indicators, Learn from social leaders and Set goals and baselines for performance and growth based on your direct competitors." />
+    <meta property="og:site_name" content="Benchmarks" />
+    <meta property="article:published_time" content="2017-12-30T05:59:00+01:00" />
+    <meta property="article:modified_time" content="2017-12-30T05:59:00+01:00" />
+    <meta property="fb:admins" content="10206916243304374" />
+
     <META NAME="geo.position" CONTENT="36.880750; 10.260194">
     <META NAME="geo.placename" CONTENT="KNSD">
     <META NAME="geo.region" CONTENT="Tunisia Tunis la Soukra 2036">
